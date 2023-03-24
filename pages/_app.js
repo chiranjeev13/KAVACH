99ee432a5c5e59de159d0fa99c845ed3d551521b
Pranshu Aggarwal <pranshu1902@gmail.com>
2023-03-24 17:56:12 +0530

    debug stash

--- pages/_app.js
+++ pages/_app.js
@@ -5,16 +5,6 @@ import '../styles/globals.css';
 
 const MyApp = ({ Component, pageProps }) => (
   <>
-<<<<<<< Updated upstream
-    <Head>
-      <title>KAVACH | Team Bitcakes</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/favicon.ico" />
-      <link rel="preconnect" href="https://stijndv.com" />
-      <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" />
-    </Head>
-    <Component {...pageProps} />
-=======
     <AppProvider>
       <Head>
         <title>KAVACH</title>
@@ -25,7 +15,6 @@ const MyApp = ({ Component, pageProps }) => (
       </Head>
       <Component {...pageProps} />
     </AppProvider>
->>>>>>> Stashed changes
   </>
 );
 
